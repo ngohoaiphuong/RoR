4daca022e85c61056773b4e6e38856ac0c3a2995
Ngo Hoai Phuong <ngohoai.phuong@gmail.com>
2012-08-01 23:25:39 +0700

    change " sign to ' sign

--- TPL_Demo/app/helpers/application_helper.rb
+++ TPL_Demo/app/helpers/application_helper.rb
@@ -11,19 +11,19 @@ module ApplicationHelper
   def get_link(page)
     link = '#'
     case page
-    when "home"
-      link = "#"
-    when "about"
-      link = "#"
-    when "contact"
-      link = "#"
-    when "signin"
-      link = "#"
+    when 'home'
+      link = '#'
+    when 'about'
+      link = '#'
+    when 'contact'
+      link = '#'
+    when 'signin'
+      link = '#'
     end
-    "#{link}"
+    '#{link}'
   end
   
   def defaultTitle
-    "TPL Demo"
+    'TPL Demo'
   end
 end
