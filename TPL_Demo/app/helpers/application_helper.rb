6eadb9bfee1a48bbf3dca2fd8fbcf68d892a1294
Ngo Hoai Phuong <ngohoai.phuong@gmail.com>
2012-08-01 23:16:06 +0700

    Ruby & Rails application
    - basic layout

---
+++ TPL_Demo/app/helpers/application_helper.rb
@@ -0,0 +1,29 @@
+module ApplicationHelper
+  def full_title(title)
+    base_title = defaultTitle
+    if(title.empty?)
+      base_title
+    else
+      "#{base_title} | #{title}"
+    end
+  end 
+  
+  def get_link(page)
+    link = '#'
+    case page
+    when "home"
+      link = "#"
+    when "about"
+      link = "#"
+    when "contact"
+      link = "#"
+    when "signin"
+      link = "#"
+    end
+    "#{link}"
+  end
+  
+  def defaultTitle
+    "TPL Demo"
+  end
+end
---
+++ TPL_Demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TPLDemo::Application.config.secret_token = '1b62cd2f222ac990543c474e3a6549efb7a3fde43151bbf77ae1610f84fdae4675b50226a53cc9e29f2f3b71a8b2548034cede69126b418e44075fae98613626'
